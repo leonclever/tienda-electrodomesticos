fc5206bdc7918365d32c93b339ff05a2f6686c2b
leonclever <83265624+leonclever@users.noreply.github.com>
2024-05-27 15:09:11 -0500

    Add files via upload

---
+++ electrodomesticos/src/main/java/com/tienda/electrodomesticos/ElectrodomesticosApplication.java
@@ -0,0 +1,13 @@
+package com.tienda.electrodomesticos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectrodomesticosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectrodomesticosApplication.class, args);
+	}
+
+}
---
+++ electrodomesticos/src/test/java/com/tienda/electrodomesticos/ElectrodomesticosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.electrodomesticos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectrodomesticosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
